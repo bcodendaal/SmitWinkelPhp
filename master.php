c4cab9313ab13a961f75057196309818f0d3eb99
bcodendaal <bcodendaal@gmail.com>
2014-08-09 10:19:57 +0200

    styling - working on left images

--- master.php
+++ master.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
-    <title>Sound Bites Society</title>
+    <title>Smit & Winkel</title>
     <title></title>
     <link href="Theme/CSS/main.css" rel="stylesheet" type="text/css"/>
     <script src="Scripts/jquery-1.7.1.min.js" type="text/javascript"></script>
@@ -42,11 +42,34 @@
             <div class="content-container container">
 
                     <div class="content-imgBoxWrapper clearfix">
-                        <div class="content-imgBox imgBox1">
-                        </div>
+                        
+                        <a class="content-imgBox imgBox1">
+                            <span class="box1text boxText">
+                                <h4>Book an appointment</h4>
+                                Click here and fill in a form to make an appointment
+                            </span>
+                        </a>
+                            
                         <div class="content-imgBox imgBox2">
+                            <span class="box2text boxText">
+                                <h4>New Patient?</h4>
+                                Save some time and fill in a New Patient Registration Form online
+                            </span>
                         </div>
                         <div class="content-imgBox imgBox3">
+                            <span class="box3text boxText">
+                                <h4>Office Hours</h4>
+                                Monday-Friday:
+                                <br/>
+                                7h30-17h30
+                                <br/>
+                                <br/>
+                                Saturdays: 
+                                <br/>
+                                (by appointment only) 
+                                <br/>
+                                7h30-11h30
+                            </span>
                         </div>
                     </div>
                     <div class="content-pageWrapper clearfix">

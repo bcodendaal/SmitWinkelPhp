291806e9641780703080c7de6d81c70ec17f1051
Pieter Roodt <pieter.roodt@gmail.com>
2014-08-13 23:15:22 +0200

    added toggle button for language

--- Pages/headerContent_EN.php
+++ Pages/headerContent_EN.php
@@ -4,6 +4,20 @@
    Tel: 021 919 5559<br>2 Mountainview Drive<br>Bellville
 </div>
 <div class="translate-container clearfix">
-  <a  class="btn-translation" href="/af">Afrikaans</a>
+     <?php if($currLang == "en"){ ?>
+    <form method="POST" action="">   
+                <input type="hidden" name="langtoggle" value="af" />
+                <input  class="btn-translation" type="submit"  value="Afrikaans" />
+            </form>
+        <?php } if($currLang == "af") { ?>
+         <div >
+             <form method="POST" action="">   
+                 <input type="hidden" name="langtoggle" value="en" />
+                <input  class="btn-translation" type="submit" value="English"   />
+            </form>
+        </div>
+        
+        <?php }  ?>
+  <!--<a  class="btn-translation" href="/af">Afrikaans</a> -->
 </div>
 <img style="display: none" src="Theme/Images/greenBtnClick.png" alt=""/>
--- master.php
+++ master.php
@@ -1,55 +1,66 @@
+ <?php
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
+        if (!array_key_exists('lang', $_SESSION)) {
+            $currLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+            $_SESSION['lang'] = $currLang;
+        }
+        if ($_SESSION['lang'] == "") {
+            $_SESSION['lang'] = "en";
+            $currLang = "en";
+        } else {
+            $currLang = $_SESSION['lang'];
+            if(isset($_POST))
+            {
+                if(array_key_exists('langtoggle',$_POST))
+                {
+                $currLang = $_POST['langtoggle'];
+                $_SESSION['lang'] = $currLang;
+                }
+            }
+        }
+        
+        
+        ?>
+        
 <!DOCTYPE html>
 <html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-    <title>Smit & Winkel</title>
-    <title></title>
-    <link href="Theme/CSS/main.css" rel="stylesheet" type="text/css"/>
-    <script src="Scripts/jquery-1.7.1.min.js" type="text/javascript"></script>
-</head>
-<body>
-    <?php 
-    if(!isset($_SESSION)) { session_start(); } 
-    
-    if(!array_key_exists ( 'lang' , $_SESSION))
-    {
-        $currLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-	$_SESSION['lang'] = $currLang; 
-    }
-	   if ($_SESSION['lang'] == "") { 
-	$_SESSION['lang'] = "en"; 
-	$currLang = "en"; 
-	} else { 
-	$currLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); 
-	$_SESSION['lang'] = $currLang; 
-	} 
-?>
-    <div class="page-wrapper">
-        <div id="header" class="header-wrapper clearfix">
-            <div class="header-container container clearfix">
-                <?php include('Pages/headerContent_EN.php') ?>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <title>Smit & Winkel</title>
+        <title></title>
+        <link href="Theme/CSS/main.css" rel="stylesheet" type="text/css"/>
+        <script src="Scripts/jquery-1.7.1.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="page-wrapper">
+            <div id="header" class="header-wrapper clearfix">
+                <div class="header-container container clearfix">
+<?php include('Pages/headerContent_EN.php') ?>
+                </div>
             </div>
-        </div>
-        <div id="navigation">
-            <?php include('Pages/navContent_EN.php') ?>
-        </div>
-        <div id="content" class="content-wrapper">
-            <div class="content-container container">
+            <div id="navigation">
+<?php include('Pages/navContent_EN.php') ?>
+            </div>
+            <div id="content" class="content-wrapper">
+                <div class="content-container container">
 
                     <div class="content-imgBoxWrapper clearfix">
-                        
+
                         <a href="#" class="content-imgBox imgBox1">
                             <span class="box1text boxText">
                                 <h4>Book an appointment</h4>
                                 Click here and fill in a form to make an appointment
                             </span>
                         </a>
-                            
+
                         <a href="#" class="content-imgBox imgBox2">
                             <span class="box2text boxText">
                                 <h4>New Patient?</h4>
@@ -73,66 +84,66 @@
                         </a>
                     </div>
                     <div class="content-pageWrapper clearfix">
-                        <?php include($page_content);?>
+<?php include($page_content); ?>
                     </div>
-                
-                <div class="smallImages-wrapper">
-                    <div class="smallImageContainer">
-                         <a>
-                            <div>
-                                <img src="Theme/Images/smallImage1.png" class="smallImage" alt=""/>
-                            </div>
-                           <span class="smallImage-text">
-                                <h4>Book an appointment</h4>
-                                Click here and fill in a form to make an appointment
-                            </span>
-                        </a>
-                    </div>
-                    <div style="clear:both">&nbsp;</div>
-                    <div class="smallImageContainer">
-                        <a>
-                            <div>
-                                <img src="Theme/Images/smallImage1.png" class="smallImage" alt=""/>
-                            </div>
-                            <span class="smallImage-text">
-                                <h4>New Patient?</h4>
-                                Save some time and fill in a New Patient Registration Form online
-                            </span>
-                        </a>
-                    </div>
-                    <div style="clear:both">&nbsp;</div>
-                  <div class="smallImageContainer">
-                        <a>
-                            <div>
-                                <img src="Theme/Images/smallImage1.png" class="smallImage" alt=""/>
-                            </div>
-                            <span class="smallImage-text">
 
-                                <h4>Office Hours</h4>
+                    <div class="smallImages-wrapper">
+                        <div class="smallImageContainer">
+                            <a>
+                                <div>
+                                    <img src="Theme/Images/smallImage1.png" class="smallImage" alt=""/>
+                                </div>
+                                <span class="smallImage-text">
+                                    <h4>Book an appointment</h4>
+                                    Click here and fill in a form to make an appointment
+                                </span>
+                            </a>
+                        </div>
+                        <div style="clear:both">&nbsp;</div>
+                        <div class="smallImageContainer">
+                            <a>
+                                <div>
+                                    <img src="Theme/Images/smallImage1.png" class="smallImage" alt=""/>
+                                </div>
+                                <span class="smallImage-text">
+                                    <h4>New Patient?</h4>
+                                    Save some time and fill in a New Patient Registration Form online
+                                </span>
+                            </a>
+                        </div>
+                        <div style="clear:both">&nbsp;</div>
+                        <div class="smallImageContainer">
+                            <a>
+                                <div>
+                                    <img src="Theme/Images/smallImage1.png" class="smallImage" alt=""/>
+                                </div>
+                                <span class="smallImage-text">
 
-                                Monday-Friday:
-                                <br/>
-                                7h30-17h30
-                                <br/>
+                                    <h4>Office Hours</h4>
 
-                                Saturdays: 
-                                <br/>
-                                (by appointment only) 
-                                <br/>
-                                7h30-11h30
-                            </span>
-                            <div style="clear:both">&nbsp;</div>
-                        </a>
+                                    Monday-Friday:
+                                    <br/>
+                                    7h30-17h30
+                                    <br/>
+
+                                    Saturdays: 
+                                    <br/>
+                                    (by appointment only) 
+                                    <br/>
+                                    7h30-11h30
+                                </span>
+                                <div style="clear:both">&nbsp;</div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div id="footer" class="footer-wrapper">
-            <div class="footer-container container">
+            <div id="footer" class="footer-wrapper">
+                <div class="footer-container container">
+                </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
 
 <script src="Scripts/Common.js" type="text/javascript"></script>
